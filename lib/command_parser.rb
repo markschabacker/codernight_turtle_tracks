513c4778e87f9590f4ed078a809ecf0bddcedfd1
Mark Schabacker <mark.schabacker@gmail.com>
2013-07-31 22:41:41 -0400

    Add support for REPEATs in CommandParser

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -3,17 +3,40 @@ require 'command_backward'
 
 class CommandParser
   def parse(input)
-    input.split("\n").inject([]) do |results, line|
-      line_command = line.split(" ")
-      results << self.send(line_command.shift, line_command)
-    end
+    tokens = input.split(" ")
+    result = []
+    parse_rec(tokens, result)
+    result.flatten
   end
 
 private
+  def parse_rec(tokens, result)
+    unless tokens.nil? || tokens.count <= 0
+      this_result, remaining_args = self.send(tokens[0], tokens[1..-1])
+      result << this_result
+
+      parse_rec(remaining_args, result)
+    end
+  end
+
   def FD(args)
-    CommandForward.new(args[0].to_i)
+    [CommandForward.new(args[0].to_i), args[1..-1]]
   end
   def BK(args)
-    CommandBackward.new(args[0].to_i)
+    [CommandBackward.new(args[0].to_i), args[1..-1]]
+  end
+
+  def REPEAT(args)
+    # TODO: this won't work w/ recursive REPEATs
+    bracket_start = 2
+    bracket_end = args.index("]")
+
+    repeated_command = args[bracket_start...bracket_end].join(" ")
+    commands = args[0].to_i.times.map do
+      parse(repeated_command)
+    end
+
+    remaining_args = args[(bracket_end + 1)..-1]
+    [commands, remaining_args]
   end
 end
--- spec/lib/command_parser_spec.rb
+++ spec/lib/command_parser_spec.rb
@@ -33,5 +33,67 @@ describe "The Command Parser" do
       command_list[2].should be_a(CommandForward)
       command_list[2].distance.should == 42
     end
+
+    describe "Correctly repeats with the REPEAT command" do
+      it "for a single command" do
+        repeat_count = 5
+        distance = 12
+        input = "REPEAT #{repeat_count} [ FD #{distance} ]"
+
+        command_list = parser.parse(input)
+
+        command_list.count.should == repeat_count
+        (0...repeat_count).each do |i|
+          command_list[i].should be_a(CommandForward)
+          command_list[i].distance.should == distance
+        end
+      end
+
+      it "for multiple commands" do
+        repeat_count = 5
+        distance_forward = 12
+        distance_backward = 7
+        input = "REPEAT #{repeat_count} [ FD #{distance_forward} BK #{distance_backward} ]"
+
+        command_list = parser.parse(input)
+
+        command_list.count.should == repeat_count * 2
+        (0...repeat_count).each do |count|
+          i = count * 2
+          command_list[i].should be_a(CommandForward)
+          command_list[i].distance.should == distance_forward
+          command_list[i + 1].should be_a(CommandBackward)
+          command_list[i + 1].distance.should == distance_backward
+        end
+      end
+    end
+
+    it "can parse a complex command" do
+      input = "FD 0\n"\
+              "BK 1\n"\
+              "REPEAT 2 [ FD 3 BK 4 ]\n"\
+              "FD 5\n"\
+              "BK 6\n"
+
+      command_list = parser.parse(input)
+
+      command_list.count.should == 8
+      command_list[0].should be_a(CommandForward)
+      command_list[0].distance.should == 0
+      command_list[1].should be_a(CommandBackward)
+      command_list[1].distance.should == 1
+      command_list[2].should be_a(CommandForward)
+      command_list[2].distance.should == 3
+      command_list[3].should be_a(CommandBackward)
+      command_list[3].distance.should == 4
+      command_list[4].should be_a(CommandForward)
+      command_list[4].distance.should == 3
+      command_list[5].should be_a(CommandBackward)
+      command_list[5].distance.should == 4
+      command_list[6].should be_a(CommandForward)
+      command_list[6].distance.should == 5
+      command_list[7].should be_a(CommandBackward)
+      command_list[7].distance.should == 6
+    end
   end
 end
