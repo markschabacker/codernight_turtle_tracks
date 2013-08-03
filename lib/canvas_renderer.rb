9f6045625cc83d939eaa92944b14dcd4964f5b83
Mark Schabacker <mark.schabacker@gmail.com>
2013-08-03 12:29:42 -0400

    Improve CanvasRenderer.render performance
    - New algorithm is less elegant but much faster for complex programs

--- lib/canvas_renderer.rb
+++ lib/canvas_renderer.rb
@@ -1,9 +1,26 @@
 class CanvasRenderer
   def render(canvas_size, points)
+    canvas_grid = create_grid(canvas_size)
+    fill_points(canvas_grid, points)
+    convert_to_string(canvas_grid)
+  end
+
+private
+  def create_grid(canvas_size)
     canvas_size.times.map do |y|
       canvas_size.times.map do |x|
-        points.any? { |point| point.x == x && point.y == y } ? "X" : "."
-      end.join(" ")
-    end.reverse.join("\n")
+        "."
+      end
+    end
+  end
+
+  def fill_points(canvas_grid, points)
+    points.each do |point|
+      canvas_grid[point.y][point.x] = "X"
+    end
+  end
+
+  def convert_to_string(canvas_grid)
+    canvas_grid.map{ |row| row.join(" ")}.reverse.join("\n")
   end
 end
