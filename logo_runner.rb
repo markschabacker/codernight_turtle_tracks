90675f6ea80f3edb453fcad24c331df44fbeb1db
Mark Schabacker <mark.schabacker@gmail.com>
2013-08-22 21:55:42 -0400

    Clean up LOGORunner.evaluate

--- logo_runner.rb
+++ logo_runner.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'stringio'
 
 require_relative 'lib/canvas_renderer'
 require_relative 'lib/command_parser'
@@ -8,20 +9,26 @@ require_relative 'lib/turtle'
 
 class LOGORunner
   def evaluate(input)
-    input_lines = input.split("\n")
-
-    canvas_size = input_lines[0].to_i
-    canvas_center = canvas_size / 2
-    turtle = Turtle.new(OpenStruct.new(:x => canvas_center, :y => canvas_center))
-
-    logo_commands_text = input_lines[2..-1].join("\n")
-    logo_commands = CommandParser.new.parse(logo_commands_text)
+    parse_input(input)
+    turtle = initialize_turtle(canvas_size)
     turtle.perform_commands(logo_commands)
-
     CanvasRenderer.new.render(canvas_size, turtle.visited_points)
   end
-end
 
+private
+  attr_reader :canvas_size, :logo_commands
+
+  def parse_input(input)
+    input_io = StringIO.new(input)
+    @canvas_size = input_io.gets.chomp.to_i
+    @logo_commands = CommandParser.new.parse(input_io.read)
+  end
+
+  def initialize_turtle(canvas_size)
+    canvas_center = canvas_size / 2
+    Turtle.new(OpenStruct.new(:x => canvas_center, :y => canvas_center))
+  end
+end
 
 if __FILE__==$0
   optparse = OptionParser.new do |opts|
